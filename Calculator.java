389bbb5984f19036badd6b47ddbaa2db2a5321bb
ljain72 <53551018+ljain72@users.noreply.github.com>
2019-08-01 10:19:30 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+package com.epsilon.training.sbcalculator.bo;
+
+public class Calculator {
+	private int ip1;
+	private int ip2;
+	public Calculator()
+	{
+		System.out.println("calculator instance getting created");
+	}
+	public int addition(int i,int j)
+	{
+		return i+j;
+	}
+	public int subtraction(int i,int j)
+	{
+		return i-j;
+	}
+	public int multiplication(int i,int j)
+	{
+		return i*j;
+	}
+	public int division(int i,int j)
+	{
+		return i/j;
+	}
+
+}
